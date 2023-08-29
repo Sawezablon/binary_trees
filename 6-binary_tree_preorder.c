87ed1502da524074252554b262faa9a1a9178556
jerrynabango <jerrynabango9@gmail.com>
2023-08-29 17:21:59 +0300

    pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -15,7 +15,10 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
+	else
+	{
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
+	}
 }
