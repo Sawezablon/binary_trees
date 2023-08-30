77dface3a10cb2d97b3e7548fadbb65a06b02d94
jerrynabango <jerrynabango9@gmail.com>
2023-08-30 08:19:42 +0300

    measures size of binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,6 +14,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		return (0);
 	}
 
-	size_t size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
-	return (size);
+	else
+	{
+		return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	}
 }
